98cec9531a27262330b31986450b9d0ede773879
Miftahul Rizki Purwonegoro <mrizkip@student.ub.ac.id>
2018-07-31 15:37:03 +0700

    add port to environment variable for heroku

--- Main.go
+++ Main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	r "github.com/mrizkip/backend-finding-dosen/routers"
 )
 
 func main() {
 	router := r.NewRouter()
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":"+os.Getenv("PORT"), router)
 }
